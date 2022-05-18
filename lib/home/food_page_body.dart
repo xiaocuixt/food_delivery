3c61fd6cbffb4388fd521e68b414edb5f06c6933
xiaocuixt <cjw624923@gmail.com>
2022-05-18 17:57:04 +0800

    add page view slider

---
+++ lib/home/food_page_body.dart
@@ -0,0 +1,162 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/icon_and_text_widget.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({Key? key}) : super(key: key);
+
+  @override
+  _FoodPageBodyState createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+  var _currentPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  double _height = 220;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currentPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 320,
+      child: PageView.builder(
+          itemCount: 5,
+          controller: pageController,
+          itemBuilder: (context, position) {
+            return _buildPageItem(position);
+          }),
+    );
+  }
+
+  Widget _buildPageItem(int index) {
+    Matrix4 matrix = new Matrix4.identity();
+    if (index == _currentPageValue.floor()) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currentTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currentTrans, 0);
+    } else if (index == _currentPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      var currentTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currentTrans, 0);
+    } else if (index == _currentPageValue.floor() - 1) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currentTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currentTrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 0);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 10, right: 10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Color(0xFF69c5df),
+                image: DecorationImage(
+                    fit: BoxFit.cover, image: AssetImage("images/food1.jpeg"))),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 140,
+              margin: EdgeInsets.only(left: 30, right: 30, bottom: 20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Color(0xFFe8e8e8),
+                        blurRadius: 5.0,
+                        offset: Offset(0, 5)),
+                    BoxShadow(color: Colors.white, offset: Offset(-5, 0)),
+                    BoxShadow(color: Colors.white, offset: Offset(5, 0))
+                  ]),
+              child: Container(
+                padding: EdgeInsets.only(top: 15, left: 15, right: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Bitter Organge Hamberger"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Wrap(
+                            children: List.generate(
+                                5,
+                                (index) => Icon(Icons.star,
+                                    color: AppColors.yellowColor))),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "4.5"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "1287"),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        SmallText(text: "comments")
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconAndTextWidget(
+                            icon: Icons.circle_sharp,
+                            text: "Normal",
+                            iconColor: AppColors.iconColor1),
+                        IconAndTextWidget(
+                            icon: Icons.location_on,
+                            text: "1.7km",
+                            iconColor: AppColors.mainColor),
+                        IconAndTextWidget(
+                            icon: Icons.access_time_rounded,
+                            text: "32min",
+                            iconColor: AppColors.iconColor2)
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
